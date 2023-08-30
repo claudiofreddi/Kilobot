9ca6d60051e7eeb7069e27b63651e874b7581036
claudiofreddi <git@claudiofreddi>
2023-08-30 23:22:17 +0200

    Joystick functioning

--- install/kilobot/share/kilobot/cmake/kilobotConfig.cmake
+++ install/kilobot/share/kilobot/cmake/kilobotConfig.cmake
@@ -1,1 +1,42 @@
-/home/claudio/dev_ws/src/kilobot/build/kilobot/ament_cmake_core/kilobotConfig.cmake
+# generated from ament/cmake/core/templates/nameConfig.cmake.in
+
+# prevent multiple inclusion
+if(_kilobot_CONFIG_INCLUDED)
+  # ensure to keep the found flag the same
+  if(NOT DEFINED kilobot_FOUND)
+    # explicitly set it to FALSE, otherwise CMake will set it to TRUE
+    set(kilobot_FOUND FALSE)
+  elseif(NOT kilobot_FOUND)
+    # use separate condition to avoid uninitialized variable warning
+    set(kilobot_FOUND FALSE)
+  endif()
+  return()
+endif()
+set(_kilobot_CONFIG_INCLUDED TRUE)
+
+# output package information
+if(NOT kilobot_FIND_QUIETLY)
+  message(STATUS "Found kilobot: 0.0.0 (${kilobot_DIR})")
+endif()
+
+# warn when using a deprecated package
+if(NOT "" STREQUAL "")
+  set(_msg "Package 'kilobot' is deprecated")
+  # append custom deprecation text if available
+  if(NOT "" STREQUAL "TRUE")
+    set(_msg "${_msg} ()")
+  endif()
+  # optionally quiet the deprecation message
+  if(NOT ${kilobot_DEPRECATED_QUIET})
+    message(DEPRECATION "${_msg}")
+  endif()
+endif()
+
+# flag package as ament-based to distinguish it after being find_package()-ed
+set(kilobot_FOUND_AMENT_PACKAGE TRUE)
+
+# include all config extra files
+set(_extras "")
+foreach(_extra ${_extras})
+  include("${kilobot_DIR}/${_extra}")
+endforeach()
--- install/kilobot/share/kilobot/launch/install/setup.sh
+++ install/kilobot/share/kilobot/launch/install/setup.sh
@@ -7,7 +7,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/claudio/dev_ws/src/kilobot/install
+_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/claudio/dev_ws/src/kilobot/launch/install
 if [ ! -z "$COLCON_CURRENT_PREFIX" ]; then
   _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX="$COLCON_CURRENT_PREFIX"
 elif [ ! -d "$_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX" ]; then
